cb615aeca415743ec5348d139ca2b9ff3dd1e8b6
Fredrik Andersson <krueger71@users.noreply.github.com>
2022-12-04 15:11:44 +0100

    First attempt at audio

--- src/emusdl2.rs
+++ src/emusdl2.rs
@@ -3,14 +3,14 @@ use std::{
     time::{Duration, Instant},
 };
 
+use crate::chip8::{Chip8, DISPLAY_HEIGHT, DISPLAY_WIDTH};
+use sdl2::audio::{AudioCallback, AudioSpecDesired};
 use sdl2::{
     event::Event,
     keyboard::{Keycode, Scancode},
     pixels::Color,
     rect::Point,
 };
-
-use crate::chip8::{Chip8, DISPLAY_HEIGHT, DISPLAY_WIDTH};
 
 /// An emulator of the Chip8 model using SDL2 for keyboard input, video and sound
 pub struct EmuSdl2 {
@@ -68,6 +68,32 @@ impl EmuSdl2 {
             canvas.output_size().unwrap()
         );
 
+        // Audio
+        let audio_subsystem = sdl.audio().unwrap();
+
+        let desired_spec = AudioSpecDesired {
+            freq: Some(44100),
+            channels: Some(1), // mono
+            samples: None,     // default sample size
+        };
+
+        let device = audio_subsystem
+            .open_playback(None, &desired_spec, |spec| {
+                // initialize the audio callback
+                SquareWave {
+                    phase_inc: 220.0 / spec.freq as f32,
+                    phase: 0.0,
+                    volume: 0.25,
+                }
+            })
+            .unwrap();
+
+        println!(
+            "{} {:?}",
+            audio_subsystem.current_audio_driver(),
+            audio_subsystem
+        );
+
         let mut events = sdl.event_pump().unwrap();
 
         'main: loop {
@@ -122,8 +148,10 @@ impl EmuSdl2 {
 
             // Decrement sound timer if non-zero and play sound
             if self.chip8.st > 0 {
-                // TODO Play sound
+                device.resume();
                 self.chip8.st -= 1;
+            } else {
+                device.pause();
             }
 
             // Draw display if Chip8 indicates display is updated
@@ -180,6 +208,28 @@ impl EmuSdl2 {
             Scancode::C => Some(0xB),
             Scancode::V => Some(0xF),
             _ => None,
+        }
+    }
+}
+
+struct SquareWave {
+    phase_inc: f32,
+    phase: f32,
+    volume: f32,
+}
+
+impl AudioCallback for SquareWave {
+    type Channel = f32;
+
+    fn callback(&mut self, out: &mut [f32]) {
+        // Generate a square wave
+        for x in out.iter_mut() {
+            *x = if self.phase <= 0.5 {
+                self.volume
+            } else {
+                -self.volume
+            };
+            self.phase = (self.phase + self.phase_inc) % 1.0;
         }
     }
 }
